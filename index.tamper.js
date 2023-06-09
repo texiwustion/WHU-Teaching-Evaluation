655e09d7ebaa2f71cd8a9c7fceb2afcf67ef9970
texiwustion <w6666s@outlook.com>
2023-06-09 23:40:02 +0800

    🐞 fix(button): 修复无法识别的button

--- index.tamper.js
+++ index.tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         武汉大学学生自动评教
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.21
 // @description  Add a floating button to execute custom JS on ugsqs.whu.edu.cn
 // @author       Texiwustion
 // @match        https://ugsqs.whu.edu.cn/*
@@ -22,9 +22,51 @@ function createFloatingButton(props) {
   const button = _createButton();
   document.body.appendChild(button);
   // Hover to changeCursor
-  button.addEventListener('mousemove', (event)=>changeCursorOnButton(event, props));
+  button.addEventListener('mousemove', (event)=>{
+    //防止移动过程中出事
+    if (!props.checkCursor) {
+      return
+    }
+    const rect = button.getBoundingClientRect();
+    const halfWidth = rect.width / 2;
+    const mouseX = event.clientX - rect.left;
+
+    if (mouseX < halfWidth) {
+      button.style.cursor = 'move';
+      props.moved = true;
+    } else {
+      button.style.cursor = 'default';
+      props.moved = false;
+    }
+  });
   // Make the button draggable
-  button.onmousedown = (event)=>dragButton(event, props);
+  button.onmousedown = (event)=>{
+    // Change position to absolute
+    button.style.position = 'absolute';
+    let shiftX = event.clientX - button.getBoundingClientRect().left;
+    let shiftY = event.clientY - button.getBoundingClientRect().top;
+    //console.log(shiftX, shiftY);
+    function moveAt(pageX, pageY) {
+      button.style.cursor = 'move';
+      button.style.left = pageX - shiftX + 'px';
+      button.style.top = pageY - shiftY + 'px';
+    }
+    function onMouseMove(event) {
+      if (props.moved) {
+        moveAt(event.pageX, event.pageY);
+        props.checkCursor = false;
+      }
+    }
+    document.addEventListener('mousemove', onMouseMove);
+    button.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      button.onmouseup = null;
+      // Change position back to fixed
+      button.style.position = 'fixed';
+      //checkCursor
+      props.checkCursor = true;
+    };
+  };
   button.ondragstart = () => false;
   // Add two div elements for the left and right halves of the button
   const leftHalf = _createLelfHalfButton();
@@ -34,20 +76,6 @@ function createFloatingButton(props) {
   button.appendChild(rightHalf);
 }
 
-function _createButton() {
-  const button = document.createElement('button');
-  button.style.position = 'fixed';
-  button.style.width = '50px';
-  button.style.height = '50px';
-  button.style.right = '20vh';
-  button.style.top = '20vh';
-  button.style.zIndex = '100000';
-  button.style.borderRadius = '50%';
-  button.style.backgroundColor = '#ffffff';
-  button.style.border = '3px solid #ccc';
-  button.style.cursor = 'move';
-  return button;
-}
 function executeCustomJS() {
   let counter = 0;
 
@@ -71,6 +99,20 @@ function executeCustomJS() {
   document.getElementById("pjsubmit").click();
 }
 
+function _createButton() {
+  const button = document.createElement('button');
+  button.style.position = 'fixed';
+  button.style.width = '50px';
+  button.style.height = '50px';
+  button.style.right = '20vh';
+  button.style.top = '20vh';
+  button.style.zIndex = '100000';
+  button.style.borderRadius = '50%';
+  button.style.backgroundColor = '#ffffff';
+  button.style.border = '3px solid #ccc';
+  button.style.cursor = 'move';
+  return button;
+}
 function _createLelfHalfButton() {
   const leftHalf = document.createElement('div');
   leftHalf.style.width = '50%';
@@ -81,7 +123,6 @@ function _createLelfHalfButton() {
   leftHalf.style.top = '0';
   return leftHalf;
 }
-
 function _createRightHalfButton() {
   const rightHalf = document.createElement('div');
   rightHalf.style.width = '50%';
@@ -92,53 +133,3 @@ function _createRightHalfButton() {
   rightHalf.style.top = '0';
   return rightHalf;
 }
-
-function changeCursorOnButton(event, props) {
-  //防止移动过程中出事
-  if (!props.checkCursor) {
-    return
-  }
-  const rect = button.getBoundingClientRect();
-  const halfWidth = rect.width / 2;
-  const mouseX = event.clientX - rect.left;
-
-  if (mouseX < halfWidth) {
-    button.style.cursor = 'move';
-    props.moved = true;
-  } else {
-    button.style.cursor = 'default';
-    props.moved = false;
-  }
-}
-
-function dragButton(event, props) {
-  // Change position to absolute
-  button.style.position = 'absolute';
-  let shiftX = event.clientX - button.getBoundingClientRect().left;
-  let shiftY = event.clientY - button.getBoundingClientRect().top;
-  //console.log(shiftX, shiftY);
-
-  function moveAt(pageX, pageY) {
-    button.style.cursor = 'move';
-    button.style.left = pageX - shiftX + 'px';
-    button.style.top = pageY - shiftY + 'px';
-  }
-
-  function onMouseMove(event) {
-    if (props.moved) {
-      moveAt(event.pageX, event.pageY);
-      props.checkCursor = false;
-    }
-  }
-
-  document.addEventListener('mousemove', onMouseMove);
-
-  button.onmouseup = function () {
-    document.removeEventListener('mousemove', onMouseMove);
-    button.onmouseup = null;
-    // Change position back to fixed
-    button.style.position = 'fixed';
-    //checkCursor
-    props.checkCursor = true;
-  };
-};
